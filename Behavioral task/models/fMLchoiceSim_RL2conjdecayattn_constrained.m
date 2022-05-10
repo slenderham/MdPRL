8d0fb21053c565046711f86a98c1fed1e6b718f7
Chong Wang <f005d7d@discovery7.hpcc.dartmouth.edu>
2022-05-09 23:59:38 -0400

    omega version not working

--- Behavioral task/models/fMLchoiceSim_RL2conjdecayattn_constrained.m
+++ Behavioral task/models/fMLchoiceSim_RL2conjdecayattn_constrained.m
@@ -33,14 +33,20 @@ else
     alpha_unr           = alpha_rew ;
 end
 
-beta_attn_choice_feat = xpar([NparamWithLR+1]);
-beta_attn_choice_conj = xpar([NparamWithLR+2]);
-if sesdata.flagSepAttn==1
-    beta_attn_learn_feat = xpar([NparamWithLR+3]);
-    beta_attn_learn_conj = xpar([NparamWithLR+4]);
+if strcmp(sesdata.attn_mode_choice, "const")
+    beta_attn_choice = 0;
+    if strcmp(sesdata.attn_mode_learn, "const")
+        beta_attn_learn = 0;
+    else
+        beta_attn_learn = xpar(NparamWithLR+1);
+    end
 else
-    beta_attn_learn_feat = beta_attn_choice_feat;
-    beta_attn_learn_conj = beta_attn_choice_conj;
+    beta_attn_choice = xpar(NparamWithLR+1);
+    if strcmp(sesdata.attn_mode_learn, "const")
+        beta_attn_learn = 0;
+    else
+        beta_attn_learn = xpar(NparamWithLR+2);
+    end
 end
 
 shapeMap        = sesdata.expr.shapeMap ;
