0e2600c652b636f0957210be4fbdc17f3982dbd9
slenderham <wc199951@gmail.com>
2024-05-10 22:53:38 -0400

    add exemplar models

---
+++ Behavioral task/models/run_fit_exemplar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=fitattn       # create a short name for your job
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=32       # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --time=150:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=fail         # send email on job start, end and fault
+#SBATCH --mail-user=chong.wang.gr@dartmouth.edu
+#SBATCH --output=slurm_%j.txt
+
+module purge
+module load matlab/r2022a
+
+matlab -nodisplay -nosplash -r Fit_Exemplar
+echo "Exemplar done"
+
